09c90d78334ba93fc7f5f914401e6c9ad4ee54b0
shah waseem <shahwaseem775@gmail.com>
2022-01-19 10:36:05 +0500

    added the routes folder

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // note actually this not a mongo project it is the CRUD project 
 const { urlencoded } = require('express');
 const config = require('config');
-const express =  require('express')
+const express =  require('express'); 
 const helmet = require('helmet');
 const morgan = require('morgan');
-const Joi = require('joi');
 const app = express();
 app.use(helmet());
 app.use(express.json())
@@ -12,19 +11,15 @@ app.set('view engine','pug')
 app.set('views','./views')
 app.use(urlencoded({extended : true}));
 const courses = require('./routes/courses');
+const home = require('./routes/home')
 app.use('/api/courses',courses)
+app.use('/',home)
 console.log(`Application Name === ${config.get('name')}`)
 console.log(`Mail server === ${config.get('mail.host')}`)
 console.log(`Mail password === ${config.get('mail.password')}`)
-
-
-if (app.get('env')=== 'development') app.use(morgan('tiny')) ,console.log("morgan enabled")
-
-app.get('/',(req,res)=>{
-    res.render('index',{title : "My express App",message : 'hello world'})
-
-});
-
-
+if (app.get('env')=== 'development')
+{
+ app.use(morgan('tiny')) ,console.log("morgan enabled")
+}
 const port = process.env.PORT || 3000
 app.listen(port , ()=> console.log(`listening on port ${port}...`))
